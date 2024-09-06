8e4cac667f3c33f63b0fa9b71cefb5601332f03f
samirh99 <rhalimsami@gmail.com>
2024-09-06 21:15:49 +0100

    Last Update CategoriesController

--- src/main/java/vinci/ma/inventory/web/controllers/AdminController/CategoriesController.java
+++ src/main/java/vinci/ma/inventory/web/controllers/AdminController/CategoriesController.java
@@ -5,12 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import vinci.ma.inventory.dao.entities.Admin;
 import vinci.ma.inventory.dao.entities.Category;
+import vinci.ma.inventory.dao.repositories.AdminRepo;
 import vinci.ma.inventory.dao.repositories.CategoryRepo;
 import vinci.ma.inventory.services.managers.CategoryManager;
 import vinci.ma.inventory.services.managers.NotificationManager;
@@ -30,7 +34,8 @@ public class CategoriesController {
     private CategoryRepo categoryRepo;
     @Autowired
     private NotificationManager notificationManager;
-
+    @Autowired
+    private AdminRepo adminRepo;
 
 
 
@@ -42,7 +47,15 @@ public class CategoriesController {
 
         model.addAttribute("categories", categories);
         model.addAttribute("badgeColors", badgeColors);
-
+        // Fetch the currently logged-in admin
+        Authentication authenticationn = SecurityContextHolder.getContext().getAuthentication();
+        String username = authenticationn.getName();
+        Admin loggedInAdmin = adminRepo.findAdminByUsername(username);
+        model.addAttribute("loggedInAdmin", loggedInAdmin);
+        if (loggedInAdmin.getAdminPicture() != null) {
+            String base64Image = Base64.getEncoder().encodeToString(loggedInAdmin.getAdminPicture());
+            model.addAttribute("adminPicture", "data:image/jpeg;base64," + base64Image);
+        }
         return "categories"; // Ensure this is the name of your Thymeleaf template file
     }
 
@@ -163,6 +176,15 @@ public class CategoriesController {
             List<Category> categories = categoryRepo.findAll();
             model.addAttribute("categories", categories);
 
+            // Fetch the currently logged-in admin
+            Authentication authenticationn = SecurityContextHolder.getContext().getAuthentication();
+            String username = authenticationn.getName();
+            Admin loggedInAdmin = adminRepo.findAdminByUsername(username);
+            model.addAttribute("loggedInAdmin", loggedInAdmin);
+            if (loggedInAdmin.getAdminPicture() != null) {
+                String base1 = Base64.getEncoder().encodeToString(loggedInAdmin.getAdminPicture());
+                model.addAttribute("adminPicture", "data:image/jpeg;base64," + base1);
+            }
             return "categoryEdit"; // The name of your HTML template
         }
 
